ae6988405099e44fe8ce37b8a8f41571e334bbbe
Christopher Pooya Razavian <pooya72@gmail.com>
2019-03-27 20:53:54 +0000

    test create new book and availability in books

--- cypress/integration/create_book_spec.js
+++ cypress/integration/create_book_spec.js
@@ -1,19 +1,37 @@
+const book1 = {book_title: "Cypress Book1"};
+const book2 = {book_title: "Cypress Book2", location_id: "1"};
+const book3 = {book_title: "Cypress Book2", author:"Batman"};
+
 describe('create a new book', () => {
     it('Sends a JSON and returns a JSON', () => {
         cy
-            .request('POST', 'http://127.0.0.1:3000/books/create', { book_title: "Cypress Testing API" })
+            .request('POST', 'http://127.0.0.1:3000/books/create', book1)
             .then((response) => {
             // response.body is automatically serialized into JSON
-            expect(response.body).to.have.property('book_title', "Cypress Testing API");
-        })
-    })
-})
+            expect(response.body).to.have.property('book_title', book1.book_title);
+        });
+    });
+});
 
-/*
-cy
-    .request('POST', 'http://localhost:8888/users/admin', { name: 'Jane' })
-    .then((response) => {
-        // response.body is automatically serialized into JSON
-        expect(response.body).to.have.property('name', 'Jane') // true
-    })
-    */
+describe('see if new book is available', () => {
+    it('returns JSON', () => {
+        cy.request('http://127.0.0.1:3000/books')
+            .its('headers')
+            .its('content-type')
+            .should('include', 'application/json');
+    });
+    it('should have the newly added book', () => {
+        cy.request('http://127.0.0.1:3000/books')
+          .its('body')
+          .should('include',{book_title: "Cypress Book1", location_id: null});
+      });
+    /*
+     it('should have the newly added book', () => {
+         cy.request('http://127.0.0.1:3000/books')
+         .then((response) => {
+            expect(response.body).to.include({book_title: "Cypress Book1", location_id: null});
+         });
+     });
+      */
+
+});
