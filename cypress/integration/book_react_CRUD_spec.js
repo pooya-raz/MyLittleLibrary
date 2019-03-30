a823963728ba812e8f7741cb65f22c11d7b7423e
Pooya Razavian <pooya72@gmail.com>
2019-03-30 10:01:44 +0000

    changed react proxy to work with 3000

--- cypress/integration/book_react_CRUD_spec.js
+++ cypress/integration/book_react_CRUD_spec.js
@@ -1,15 +1,25 @@
 const url = Cypress.env('react');
 describe('New Book Page', () => {
+    beforeEach(() => {
+        cy.visit(url + '/new_book');
+    })
     it('should autofocus on the formTitle', () => {
         cy
-            .visit(url + '/new_book')
             .focused()
             .should('have.id', 'formTitle');
         });
     it('accepts input', () => {
-        cy.visit(url + '/new_book');
-        cy.get('.formTitle');
-
+        const typedText = 'Cypress is Awesome!';
+        cy.get('#formTitle')
+          .type(typedText)
+          .should('have.value', typedText);
+    });
+    context('Form Submission', () => {
+        it('Should submit a form', () => {
+            cy.get('#formTitle')
+            .type('Cypress is Awesome!')
+            .type('{enter}')
+        })
     })
 
     });
