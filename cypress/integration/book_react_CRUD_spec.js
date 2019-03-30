966b9a65f75699f9173aaf19e8fff375fe2c7a3a
Pooya Razavian <pooya72@gmail.com>
2019-03-30 06:58:21 +0000

    created script for travis-ci

--- cypress/integration/book_react_CRUD_spec.js
+++ cypress/integration/book_react_CRUD_spec.js
@@ -1,5 +1,5 @@
 describe('Visit the homepage', () => {
-    it.only('have the name MyLittleLibrary', () => {
+    it('have the name MyLittleLibrary', () => {
         cy
             .visit('http://127.0.0.1:3001/new_book')
             .focused()
