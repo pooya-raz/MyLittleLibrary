ab0b550968c66ad723e3a70746cebabb11bb4f87
Pooya Razavian <pooya72@gmail.com>
2019-03-31 23:23:14 +0100

    book crud tests for cypress

--- cypress/integration/book_react_CRUD_spec.js
+++ cypress/integration/book_react_CRUD_spec.js
@@ -1,25 +1,38 @@
-const url = Cypress.env('react');
+import { isContext } from "vm";
 
-describe('New Book Page', () => {
-    beforeEach(() => {
-        cy.visit(url + '/new_book');
+const url = Cypress.env('react');
+var api_response = {};
+describe('User wants to create a new book', () => {
+    it('Begins at the homepage', ()=> {
+        cy.visit(url);
+    });
+    it("Clicks on the 'add book' button", () => {
+        cy.contains('Add Book').click();
+        cy.url().should('include', '/books/create'); 
     });
     it('should autofocus on the formTitle', () => {
         cy
             .focused()
             .should('have.id', 'formTitle');
     });
-    it('accepts input', () => {
-        const typedText = 'Cypress is Awesome!';
-        cy.get('#formTitle')
-            .type(typedText)
-            .should('have.value', typedText);
-    });
-    context('Form Submission', () => {
-        it('Should submit a form', () => {
-            cy.get('#formTitle')
-                .type('Cypress is Awesome!')
-                .type('{enter}');
-        });
+    it('submit a form', () => {
+        const fullurl = url + '/api/books/create';
+        const book_title = 'Cypress is Awesome!';
+        cy.server();
+        cy.route('POST', fullurl).as('create');
+        cy.get('#formTitle').as('formSubmit')
+            .type(book_title)
+            .type('{enter}');
+        cy.wait('@create').then(function (response) {
+                api_response = response;
+                expect(response.body).to.have.property('book_title',book_title) ;
     });
 });
+    it('then redirect to the book page', () => {
+        cy.location('pathname').should('eq', '/books/'+api_response.book_id);
+    });
+    it('should have the new data entered', () => {
+        cy.get('#bookTitle');
+    })
+});
+
