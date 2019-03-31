c8e0dfcc2b9cab065a66a70c06608dba5a12ec19
Pooya Razavian <pooya72@gmail.com>
2019-03-31 20:13:03 +0100

    added jshintrc to enable es6 checking

--- cypress/integration/book_react_CRUD_spec.js
+++ cypress/integration/book_react_CRUD_spec.js
@@ -1,25 +1,25 @@
 const url = Cypress.env('react');
+
 describe('New Book Page', () => {
     beforeEach(() => {
         cy.visit(url + '/new_book');
-    })
+    });
     it('should autofocus on the formTitle', () => {
         cy
             .focused()
             .should('have.id', 'formTitle');
-        });
+    });
     it('accepts input', () => {
         const typedText = 'Cypress is Awesome!';
         cy.get('#formTitle')
-          .type(typedText)
-          .should('have.value', typedText);
+            .type(typedText)
+            .should('have.value', typedText);
     });
     context('Form Submission', () => {
         it('Should submit a form', () => {
             cy.get('#formTitle')
-            .type('Cypress is Awesome!')
-            .type('{enter}');
+                .type('Cypress is Awesome!')
+                .type('{enter}');
         });
     });
-
-    });
+});
