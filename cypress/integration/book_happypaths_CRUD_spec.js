1d86eaebfa31f0f41bf6d70efd23d0eff4d6ec5b
Pooya Razavian <pooya72@gmail.com>
2019-04-01 12:21:42 +0100

    added book read e2e test

--- cypress/integration/book_happypaths_CRUD_spec.js
+++ cypress/integration/book_happypaths_CRUD_spec.js
@@ -1,6 +1,13 @@
 const url = Cypress.env('react');
+
+//This is the book that is populated from the API response
+// It is then passed around to other tests to make sure correct
+// details are returned
+let response_book= {
+    book_id: 0,
+    book_title: ""
+};
 describe('User wants to create a new book', () => {
-    let book_id = 0;
     it('Begins at the homepage', () => {
         cy.visit(url);
     });
@@ -22,14 +29,28 @@ describe('User wants to create a new book', () => {
             .type('{enter}');
 
         cy.wait('@create').then(response => {
-            book_id = response.response.body.book_id;
+            response_book.book_id = response.response.body.book_id;
+            response_book.book_title = response.response.body.book_title;
         });
     });
     it('then redirect to the book page', () => {
-        cy.location('pathname').should('eq', `/books/${book_id}`);
+        cy.location('pathname').should('eq', `/books/${response_book.book_id}`);
     });
     it('should have the new data entered', () => {
         cy.get('#bookTitle');
-    })
+    });
+
+
 });
+
+describe('User wants to see details of a book', () => {
+    it('directly visits the book url', () => {
+        cy.visit(url + `/books/${response_book.book_id}`);
+    });
+    it('has the detail of the book', () => {
+        cy.get('#bookTitle')
+        .contains(response_book.book_title);
+    });
+});
+
 
