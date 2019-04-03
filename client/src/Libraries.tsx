a62c82551de7a24bf075a4f093a2fb919fc8162a
Pooya Razavian <pooya72@gmail.com>
2019-04-03 09:00:16 +0100

    Confirms book deletion and hides confirmation after 4 seconds

--- client/src/Libraries.tsx
+++ client/src/Libraries.tsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { RouteComponentProps} from 'react-router';
 import SearchBar from './SearchBar';
 import LibraryList from './LibraryList';
 import LibraryDropdown from './LibraryDropdown';
@@ -8,12 +9,28 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Alert from 'react-bootstrap/Alert'
 
-class Libraries extends Component {
+//this is necessary so typescipt knows props has location
+interface MyProps extends RouteComponentProps {
+}
+
+class Libraries extends Component<MyProps> {
+  state={
+    showDeleteConfirmation: false 
+  }
+  componentDidMount(){
+    if (this.props.location.state !== undefined && this.props.location.state.showDeleteConfirmation === true){
+      this.setState({ showDeleteConfirmation: true })
+      let timer = () => {
+        this.setState({ showDeleteConfirmation: false})
+      }
+      setTimeout(timer, 4000);
+    }
+  }
   render() {
     return (
       <div>
         <div>
-          <Alert variant={"success"}>
+          <Alert variant={"success"} show={this.state.showDeleteConfirmation}>
             Book was successfully deleted
           </Alert>
         </div>
