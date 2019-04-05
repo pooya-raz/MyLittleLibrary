1de1bc8efc919b307031113c60443e86dcb9b046
Pooya Razavian <pooya72@gmail.com>
2019-04-05 11:52:11 +0100

    Create a Book class
    Create a Book class that can be passed to create an Book object, check types and that
    everything is well formed. This is in its own file. I wanted to use
    interfaces and avoid classes, but it seems this is the way to go, since
    you can't create an instance of an interface.

---
+++ src/books/Book.ts
@@ -0,0 +1,27 @@
+interface IndustryIdentifier {
+    type: string,
+     identifier: string 
+}
+
+export class Book {
+    id: number;
+    title: string;
+    location_id: number;
+    authors: Array<string>;
+    industryIdentifier: Array<IndustryIdentifier>;
+    published_date: number;
+    publisher: string;
+    image_url: string
+
+    constructor(bookResponse:any ){
+        this.id = bookResponse.id;
+        this.title = bookResponse.title;
+        this.location_id = bookResponse.location_id;
+        this.authors = bookResponse.authors;
+        this.industryIdentifier = bookResponse.industryIdentifier;
+        this.published_date = bookResponse.published_date;
+        this.publisher = bookResponse.publisher;
+        this.image_url = bookResponse.image_url;
+
+    }
+}
